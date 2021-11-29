4ee59fc71a3cf13c526186ee4a0417e99b85b144
Alberto Gómez Peña <albertogomp@gmail.com>
2021-11-29 23:31:13 +0100

    added search components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import Search from "./views/Search";
 
-const jsonData = [
-{ key: "1",  name: "Johan Paul Montoya"},
-{ key: "2",  name: "Alam Brito Delgado"},
-{ key: "3",  name: "Chiquito of the Road"},
-{ key: "4",  name: "Alberto Gomez"},
-{ key: "5",  name: "Worki Jimenez"},
-{ key: "6",  name: "Manolo of the Drum"}]
-
-const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 4000)
-  }, [])
-
-  const renderData =  () => {     
-    return jsonData?.map((value) => (
-      <div key={value.key}>
-        <p>{value.name}</p>
-      </div>
-    ));
-  };
-
-  return (
-    <div>
-      { isLoading ? <h2>lodin</h2> :  renderData() }      
-    </div>
-  )
-}
+const App = () => <Search /> 
 export default App;
---
+++ src/views/Search/index.js
@@ -0,0 +1,9 @@
+import SearchBox from "./SearchBox";
+
+export default function Search() {
+    return (
+        <div> 
+            <SearchBox />
+        </div>
+    );
+}
