8c268b17398782eaf1780189e6f065a86bb26309
Alberto Gómez Peña <albertogomp@gmail.com>
2021-12-04 01:19:04 +0100

    implemented api fetch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,25 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home from "../views/Home"
+import Login from "../views/Login"
+import NotFound from "../views/NotFound"
+import Search from "../views/Search"
 export default function Routes() {
     return (
         <Router>
-            <Route path="/" exact>
-                <Home />
-            </Route>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/Login" >
+                    <Login />
+                </Route>
+                <Route path="/Search" >
+                    <Search />
+                </Route>
+                <Route >
+                    <NotFound />
+                </Route>
+            </Switch>
         </Router>
     )
 
--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
 import "./style.css"
-import data from "../../data/users.json"
 import SearchResults from "./components/SearchResults";
 
 export default function Search() {
     const [isAtTop, setIstAtTop] = useState(false);
-    const [userData, setUsersData] = useState(data);
-    const [results, setResults] = useState([]);
+    const [userData, setUsersData] = useState("");
+    const [data, setData] = useState([])
+    const [results, setResults] = useState(data);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users")
+            const data = await response.json();
+            setData(data)
+        };
+
+        getUsers().catch(null);
+    }, []);
 
     const handleSearchClick = (searchText) => {
         setIstAtTop(true);
-        if (userData?.length) {
+        if (data?.length) {
             const searchTextCleaned = searchText.toLowerCase()
-            const filteredData = userData.filter((value) => (
+            const filteredData = data.filter((value) => (
                 value.name.toLowerCase().includes(searchTextCleaned) || 
                 value.phone.toLowerCase().includes(searchTextCleaned) ||
                 value.username.toLowerCase().includes(searchTextCleaned) ||
